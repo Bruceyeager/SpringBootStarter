0d0a93611a9604b7c0d49aade239ae7dc5723e5f
Bruceyeager <brucer1229@outlook.com>
2025-01-24 16:42:49 -0600

    Delete expense-tracker-app directory

--- expense-tracker-app/src/main/java/bruce/ren/expense/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package bruce.ren.expense.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
